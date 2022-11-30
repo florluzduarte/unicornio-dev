b6336b7cfb7770773fc4a67555bda221026cae11
Florencia Luz Duarte <florluzduarte@gmail.com>
2022-11-30 13:15:24 -0300

    Initial setup completed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://unicornio.dev",
+  integrations: [sitemap(), image()]
+});
