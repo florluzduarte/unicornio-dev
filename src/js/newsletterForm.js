731abd9c1be746bb34b93e0000b53081b8475c53
Florencia Luz Duarte <florluzduarte@gmail.com>
2022-12-11 22:22:20 -0300

    teaser page: form onload changes

--- src/js/newsletterForm.js
+++ src/js/newsletterForm.js
@@ -2,25 +2,28 @@ import { supabase } from "../config/supabaseClient";
 
 const form = document.querySelector("#newsletter-form");
 
-form.addEventListener("submit", async (event) => {
-  event.preventDefault();
-  const formInputs = document.querySelectorAll("input");
+window.addEventListener(
+  "load",
+  form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const formInputs = document.querySelectorAll("input");
 
-  let submission = {};
+    let submission = {};
 
-  formInputs.forEach((element) => {
-    const { value, name } = element;
-    if (value) {
-      submission[name] = value;
-    }
-  });
+    formInputs.forEach((element) => {
+      const { value, name } = element;
+      if (value) {
+        submission[name] = value;
+      }
+    });
 
-  await supabase.from("newsletter-subscriptions").insert([submission]);
-  window.location.href = "/success-newsletter";
+    await supabase.from("newsletter-subscriptions").insert([submission]);
+    window.location.href = "/success-newsletter";
 
-  const userName = document.getElementById("name");
-  userName.value = "";
+    const userName = document.getElementById("name");
+    userName.value = "";
 
-  const userEmail = document.getElementById("email");
-  userEmail.value = "";
-});
+    const userEmail = document.getElementById("email");
+    userEmail.value = "";
+  })
+);
