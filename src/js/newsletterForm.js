c0348ca719033351fe8690df1026426fb5897c9e
Florencia Luz Duarte <florluzduarte@gmail.com>
2022-12-11 21:05:58 -0300

    Teaser page: form functionality complete

---
+++ src/js/newsletterForm.js
@@ -0,0 +1,26 @@
+import { supabase } from "../config/supabaseClient";
+
+const form = document.querySelector("#newsletter-form");
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  const formInputs = document.querySelectorAll("input");
+
+  let submission = {};
+
+  formInputs.forEach((element) => {
+    const { value, name } = element;
+    if (value) {
+      submission[name] = value;
+    }
+  });
+
+  await supabase.from("newsletter-subscriptions").insert([submission]);
+  window.location.href = "/success-newsletter";
+
+  const userName = document.getElementById("name");
+  userName.value = "";
+
+  const userEmail = document.getElementById("email");
+  userEmail.value = "";
+});
