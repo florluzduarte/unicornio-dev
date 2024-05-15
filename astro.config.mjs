b6ecdcb4ad3acb157bb9e85ed9ceb464d9351e64
Florencia Luz Duarte <florluzduarte@gmail.com>
2024-05-15 18:17:27 -0300

    chore: updated dependencies, old files deleted

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,11 @@
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 import sitemap from "@astrojs/sitemap";
-
-// https://astro.build/config
-import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://unicornio.dev",
-  integrations: [
-    sitemap({
-      filter: (page) => page !== "https://unicornio.dev/success-newsletter",
-    }),
-    image(),
-  ],
+  integrations: [sitemap({
+    filter: page => page !== "https://unicornio.dev/success-newsletter"
+  })]
 });
