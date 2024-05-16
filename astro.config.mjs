a65e66754566c63ba27f7d7f5383b4b314e664c4
Florencia Luz Duarte <florluzduarte@gmail.com>
2024-05-16 17:25:59 -0300

    chore: add tailwind config + add mdx integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-
 import tailwind from "@astrojs/tailwind";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://unicornio.dev",
-  integrations: [sitemap(), tailwind()]
+  integrations: [sitemap(), tailwind(), mdx()]
 });
