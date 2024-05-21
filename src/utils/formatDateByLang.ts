e449b96d1603e75ab0dc4ccb2813b0593cedc6d2
Florencia Luz Duarte <florluzduarte@gmail.com>
2024-05-21 17:27:13 -0300

    chore: fix types any in dynamic routes blog and tutorials

--- src/utils/formatDateByLang.ts
+++ src/utils/formatDateByLang.ts
@@ -1,6 +1,10 @@
+import type { CollectionEntry } from 'astro:content';
 import type { Lang } from "@translate/dictionary";
 
-export const formatDateByLang = (page: any, lang: Lang) => {
+// Sumar cada una de las content Collections del proyecto para conseguir el tipado adecuado
+type Collections = CollectionEntry<"blog"> | CollectionEntry<"tutorials">;
+
+export const formatDateByLang = (page: Collections, lang: Lang) => {
   const formatDate = page.data.publishDate.toLocaleString(lang).split(",");
   return formatDate[0];
 };
