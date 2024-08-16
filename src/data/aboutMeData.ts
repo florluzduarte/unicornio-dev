826de556e1391d4d24201ce7631aed4e86ce5e11
Florencia Luz Duarte <florluzduarte@gmail.com>
2024-08-16 17:35:14 -0300

    feat: add About me data in english and spanish

---
+++ src/data/aboutMeData.ts
@@ -0,0 +1,212 @@
+import type { ISectionParagraph } from "@type/ui-types";
+
+export const spanishAboutMeIntro: ISectionParagraph[] = [
+  {
+    text: "¡Hola! 👋",
+    order: 1,
+  },
+  {
+    text: "Mi nombre es Florencia Luz Duarte. Soy Desarrolladora de Software especializada en Web Front-End con un background de +10 años en Diseño de Producto.",
+    order: 2,
+  },
+  {
+    text: "Me gusta generar contenido sobre Desarrollo Web bajo el nombre @unicornio.dev, formo parte de varias comunidades de tecnología y colaboro en distintos proyectos open source siempre que puedo.",
+    order: 3,
+  },
+];
+
+export const spanishCareerChange: ISectionParagraph[] = [
+  {
+    text: "Entre los 29 y 30 años decidí cambiar de carrera. La curiosidad siempre me ha llevado por caminos inesperados y este caso no fue la excepción. En ese momento estaba recién graduada de una Especialización en Gestión de Tecnología, y fue allí donde empecé a conocer más sobre el mundo del Software.",
+    "order": 1,
+  },
+  {
+    text: "Empecé a buscar cómo aprender de forma autodidacta hasta que el algoritmo de YouTube me llevó a los videos de una escuela de programación llamada apx.school, me sumé a su comunidad y el resto es historia.",
+    order: 2,
+  },
+  {
+    text: "Lo que empezó con unas líneas de HTML y CSS, terminó en una nueva dirección de carrera y en un cambio en general para mi vida.",
+    order: 3,
+  }
+];
+
+export const spanishSelfTaught: ISectionParagraph[] = [
+  {
+    text: "Durante mis 20s estudié y me recibí como Diseñadora Industrial en la UBA, y tuve la chance de ser docente por 3 años en esa misma institución.",
+    order: 1,
+  },
+  {
+    text: "Cuando me propuse cambiar de carrera tuve que decidir entre volver a la Universidad o armar mi propio camino, y finalmente ganó la segunda opción.",
+    order: 2,
+  },
+  {
+    text: "Para esta nueva etapa, preferí tomar el camino autodidacta, porque se adapta más a quien soy hoy y a mis metas. De más está decir que no es para nada fácil ser tu propio guía en un mundo tan cambiante y dinámico como el del software, pero también tiene sus ventajas.",
+    order: 3,
+  },
+  {
+    text: "Armé este espacio para compartir mis experiencias, acercar tips y recursos a otros desarrolladores autodidactas, y para documentar todo mi proceso de aprendizaje y crecimiento (con sus puntos altos y bajos).",
+    order: 4,
+  }
+];
+
+export const spanishGoals: ISectionParagraph[] = [
+  {
+    text: "Hoy me motivan muchas cosas:",
+    order: 1,
+  },
+  {
+    text: "✨ Quiero ver crecer este proyecto, generando y acercando cada vez más recursos a todos ustedes.",
+    order: 2,
+  },
+  {
+    text: "✨ Busco crecer profesionalmente, desafiarme a profundizar mis experiencias como Desarrolladora y sumarme a equipos interesantes.",
+    order: 3,
+  },
+  {
+    text: "✨ Seguir profundizando mis conocimientos y compartiendo mis experiencias, lo cual estoy haciendo gracias a OSSU y su path de Ciencias de la Computación.",
+    order: 4,
+  },
+  {
+    text: "✨ A futuro me gustaría trabajar como DevRel. Creo que es dónde confluyen mejor todas mis experiencias profesionales previas. Ya veremos si lo logro 💪",
+    order: 5,
+  },
+];
+
+export const spanishHobbies: ISectionParagraph[] = [
+  {
+    text: "Soy madre orgullosa de 3 gathijas perfectas y vivo con mi prometido (espero que se transforme en esposo pronto 🤞) quien me acompaña y apoya en cada proyecto.",
+    order: 1,
+  },
+  {
+    text: "En mi tiempo libre me gusta cocinar (especialmente panes y postres). Fanática desde pequeña de leer, me encuentran siempre con un libro bajo el brazo.",
+    order: 2,
+  },
+  {
+    text: "El café forma parte de mí, lo necesito física y espiritualmente para vivir ☕️",
+    order: 3,
+  },
+  {
+    text: "Mi lugar en el mundo es Mendoza, Argentina. Espero en el futuro mudarme allí y que me adopten para siempre en la mejor provincia argentina (no acepto discusiones sobre el tema).",
+    order: 4,
+  },
+  {
+    text: "Team frío ❄️ por siempre. Cada año que pasa soporto menos el verano y crecen mis ganas de huir para siempre de Buenos Aires.",
+    order: 5,
+  },
+  {
+    text: "Espero que encuentren cosas útiles en este humilde blog o, al menos que se lleven algunas risas y un poco de motivación.",
+    order: 6,
+  },
+  {
+    text: "Hasta acá llegó mi presentación. Me costó más escribir esta líneas que codear el sitio entero 😅",
+    order: 7,
+  },
+  {
+    text: "Muchas gracias por tu visita 🫶",
+    order: 8,
+  },
+];
+
+export const englishAboutMeIntro: ISectionParagraph[] = [
+  {
+    text: "Hi there! 👋",
+    order: 1,
+  },
+  {
+    text: "My name is Florencia Luz Duarte. I'm a Software Developer specializing in Front-End Development with a background of +10 years in Product Design.",
+    order: 2,
+  },
+  {
+    text: "I love creating content about Web Development on social media as @unicornio.dev, I'm part of several tech communities and I collaborate with different open-source projects from time to time.",
+    order: 3,
+  },
+];
+
+export const englishCareerChange: ISectionParagraph[] = [
+  {
+    text: "Between my 29th and 30th birthday, I decided to make a career change. Curiosity has always taken me to unexpected paths and this case wasn't the exception. At that time, I had recently graduated with a postgraduate degree in Management of Technology and Innovation. That's when I became interested in the world of Software.",
+    "order": 1,
+  },
+  {
+    text: "I quickly began searching for ways to learn programming as a self-taught. The YouTube algorithm led me to the videos of a coding school named apx.school, I joined their online community and the rest is history.",
+    order: 2,
+  },
+  {
+    text: "What began with a few lines of HTML and CSS, end up becoming a new professional direction for me and a more generalized change in other aspects of my life.",
+    order: 3,
+  }
+];
+
+export const englishSelfTaught: ISectionParagraph[] = [
+  {
+    text: "In my 20s I graduated as an Industrial Designer from the University of Buenos Aires (UBA) and, I had the chance to be an assistant teacher in that same institution for 3 years.",
+    order: 1,
+  },
+  {
+    text: "When I started thinking about making a career change I found myself between two options: going back to college for a second degree or making my path as a self-taught. In the end, the second option was the winner.",
+    order: 2,
+  },
+  {
+    text: "For this new phase, I took the self-educated route because it adapts better to myself and my personal goals for this particular moment in my life. To be honest with you, being self-taught isn't an easy endeavor. You have to be your own guide and coach while you navigate a complex and changing software ecosystem.",
+    order: 3,
+  },
+  {
+    text: "I put together this blog to share my learning experiences, provide resources and tips for tech newcomers, and document my career journey (with its highs and lows and all the stages in between).",
+    order: 4,
+  }
+];
+
+export const englishGoals: ISectionParagraph[] = [
+  {
+    text: "Today many things motivate me:",
+    order: 1,
+  },
+  {
+    text: "✨ I wish to see this project grow by steadily generating more and more content for all of you.",
+    order: 2,
+  },
+  {
+    text: "✨ I seek to advance in my professional career by picking more challenging projects and collaborating with others.",
+    order: 3,
+  },
+  {
+    text: "✨ I want to upgrade my knowledge and keep sharing this self-taught journey with all of you by documenting my advances on the OSSU Computer Science curricula.",
+    order: 4,
+  },
+  {
+    text: "✨ I wish to get a job as a DevRel in the future because I believe this position will allow me to integrate all the different faces of my career in one position. Let's see if I make it happen! 💪",
+    order: 5,
+  },
+];
+
+export const englishHobbies: ISectionParagraph[] = [
+  {
+    text: "I'm the proud mother of 3 gorgeous and perfect cats. I live with my fiancee (I hope he gets the Husband title soon 🤞). He is my main source of support and encourages me on every project.",
+    order: 1,
+  },
+  {
+    text: "In my free time, I love cooking (especially bread and desserts), I'm a huge fan of books and I need coffee to live ☕️ .",
+    order: 2,
+  },
+  {
+    text: "My favorite place in the world is Mendoza, Argentina. I hope to move there shortly. I believe Mendoza is by far the best Argentinian province (If you've never seen a picture of this place here are a few).",
+    order: 3,
+  },
+  {
+    text: "Team Winter forever ❄️. Each year I hate summer more and more. I wish to evaporate from Buenos Aires every January.",
+    order: 4,
+  },
+  {
+    text: "I hope you find some good information in this humble blog, or at least, some motivation to continue advancing in your coding journey.",
+    order: 5,
+  },
+  {
+    text: "This is the end of my long presentation. It took far more effort to write these lines than to code the entire website 😅",
+    order: 6,
+  },
+  {
+    text: "Thanks for visiting and I hope you enjoy 🫶",
+    order: 7,
+  },
+];
+
