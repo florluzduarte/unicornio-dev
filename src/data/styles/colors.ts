8a8debaeb558f79c831156c8aa22e4ba9067b733
Florencia Luz Duarte <florluzduarte@gmail.com>
2024-08-22 17:56:27 -0300

    feat: add ResourcesGrid and ResourceCard components

---
+++ src/data/styles/colors.ts
@@ -0,0 +1,15 @@
+export const textColors = [
+  "text-yellow",
+  "text-orange",
+  "text-pink",
+  "text-purple",
+  "text-green",
+];
+
+export const hoverBorderColors = [
+  "hover:border-yellow",
+  "hover:border-orange",
+  "hover:border-pink",
+  "hover:border-purple",
+  "hover:border-green",
+];
