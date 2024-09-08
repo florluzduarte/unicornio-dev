35a0547b3039ad4951387d55e6d31de9f6e9263f
florluzduarte <florluzduarte@gmail.com>
2024-09-08 20:46:57 -0300

    feat: add SiteMap configuration and robots.txt file

--- astro.config.ts
+++ astro.config.ts
@@ -7,7 +7,16 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   site: "https://unicornio.dev",
-  integrations: [sitemap(), tailwind(), mdx(), icon()],
+  integrations: [sitemap({
+    customPages: ["https://links.unicornio.dev", "https://florluzduarte.unicornio.dev"],
+    i18n: {
+      defaultLocale: "es",
+      locales: {
+        es: "es-AR",
+        en: "en-US",
+      }
+    }
+  }), tailwind(), mdx(), icon()],
   i18n: {
     defaultLocale: "es",
     locales: ["es", "en"],
