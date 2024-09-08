b14d1f4b415ea7a19a3967bf892e2db4c3629c02
florluzduarte <florluzduarte@gmail.com>
2024-09-07 21:05:00 -0300

    feat: add OSSU posts tranlation to english

--- src/data/blog/seriesAlertData.ts
+++ src/data/blog/seriesAlertData.ts
@@ -5,3 +5,9 @@ export const ossuSpanishSeriesAlert: BlogSeriesAlert = {
   description: "Este artículo forma parte de una serie de múltiples posteos donde estoy documentando mis experiencias con la currícula de Ciencias de la Computación de Open Source Society University. Puedes ver todos los artículos de esta serie en el siguiente link 👇️",
   link: "/es/tags/ossu",
 };
+
+export const ossuEnglishSeriesAlert: BlogSeriesAlert = {
+  title: "My OSSU experience",
+  description: "This article is part of a series of posts where I’m documenting my experiences with the Computer Science curricula from Open Source Society University. You can check out all the articles in this series at the following link 👇️",
+  link: "/en/tags/ossu",
+};
